bf6d7bcbb0a550a2e67091516ec6b5e7fd314fd0
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2018-07-21 16:39:19 -0700

    chapter 3

--- hello.py
+++ hello.py
@@ -1,12 +1,27 @@
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
+from flask import Flask, render_template
+from flask import request, make_response, redirect
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/google-it')
+def go_away():
+    return redirect('http://google.com')
+
+@app.route('/cookie')
+def cookie():
     user_agent = request.headers.get('User-Agent')
     response_string = '<h1>Hello!</h1>'
     response_string += '<p>Your browser is {}</p>'.format(user_agent)
@@ -14,10 +29,10 @@ def index():
     response.set_cookie('answer','42')
     return response
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-@app.route('/google-it')
-def go_away():
-    return redirect('http://google.com')
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
