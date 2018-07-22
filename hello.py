721436e31556a9da0208e035f7ddcb7869a6fea5
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2018-07-22 07:21:48 -0700

    chapter 4

--- hello.py
+++ hello.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
-from flask import request, make_response, redirect
+from flask import Flask, render_template, session, url_for
+from flask import request, make_response, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'disco-disco-duck-I-want-2-BA-disco-duck'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you changed your name!?!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -36,6 +48,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
